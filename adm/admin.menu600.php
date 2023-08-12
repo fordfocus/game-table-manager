59f95332a5601b80ee10a49bf9b70315c74176b8
weeperzzang <weeperzzang@naver.com>
2023-08-12 23:32:39 +0900

    테이블 만들기 메뉴 추가

---
+++ adm/admin.menu600.php
@@ -0,0 +1,5 @@
+<?php
+$menu['menu600'] = array(
+    array('600000', '테이블관리', '' . GTM_ADMIN_MAKE_URL . '/form.php', 'table'),
+    array('600100', '테이블만들기', '' . GTM_ADMIN_MAKE_URL . '/form.php', 'form'),
+);
